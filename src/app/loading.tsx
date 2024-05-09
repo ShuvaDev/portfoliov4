3ba846e49ab2c848d4a4e59e58279fa0bbbebb7f
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2024-05-09 08:08:58 +0600

    loader

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,12 @@ interface loadingType {}
 const Loading: React.FC<loadingType> = () => {
   return (
     <div className="h-screen w-full flex items-center justify-center">
-      <div className="loader">Loading</div>
+      <div className="lds-ring">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   );
 };
